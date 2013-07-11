50da55999623d945ca21ad109a114ae1aecaf967
Balint Erdi <balint.erdi@gmail.com>
2013-07-11 08:35:42 +0200

    Upgrade to capistrano3 (pre)

--- Gemfile
+++ Gemfile
@@ -1,4 +1,4 @@
 source 'http://rubygems.org'
 
 gem 'sinatra',    '~> 1.4.3'
-gem 'capistrano', '~> 2.15.5'
+gem 'capistrano', '~> 3.0.0pre'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,20 @@
-set :domain,      'ideavote.balinterdi.com'
 set :application, 'ideavote'
+set :repo_url,    'git@github.com:balinterdi/ideavote.git'
+
+set :domain,      'ideavote.balinterdi.com'
 set :deploy_to,   '/var/www/apps/ideavote'
 set :user,        'ideavote'
 set :use_sudo,    false
 set :scm,         :git
-set :branch,      'separate-environments'
-set :repository,  'git@github.com:balinterdi/ideavote.git'
+# set :branch,      'separate-environments'
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+
+set :format, :pretty
+set :log_level, :debug
+# set :pty, true
+
+# set :linked_files, %w{config/database.yml}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# set :default_environment, { path: "/opt/ruby/bin:$PATH" }
+# set :keep_releases, 5
