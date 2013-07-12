2084662303ee902d170c9c84fe30f6ff7086cd78
Balint Erdi <balint.erdi@gmail.com>
2013-07-12 07:57:06 +0200

    Specify db name explicitly

--- app.rb
+++ app.rb
@@ -3,7 +3,10 @@ require 'bundler/setup'
 require 'sinatra'
 
 class App < Sinatra::Base
+
+  set :db_name, 'bcug-ideavote'
+
   get '/' do
-    erb :index, locals: { db_name: "ideavote-#{settings.environment}" }
+    erb :index, locals: { db_name: "#{settings.db_name}-#{settings.environment}" }
   end
 end
